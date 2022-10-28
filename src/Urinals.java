dcdfefe2faa580876c141d107c8fee71455abe5d
purshi01 <purushothamapurshi8@gmail.com>
2022-10-27 19:52:03 -0700

    goodString function is added

--- src/Urinals.java
+++ src/Urinals.java
@@ -1,2 +1,10 @@
 public class Urinals {
+// Author Purushothama Shanthappa
+    Boolean goodString( String str ) { // checks to see if valid string
+        System.out.println ("Not yet implemented");
+        return true;
+    }
+    public static void main(String[] args) {
+
+    }
 }
